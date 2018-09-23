01c0cfe47bff793de01ae46b39a64db265740498
Ryan Moeller <ryan@freqlabs.com>
2018-09-23 16:52:27 -0700

    Implement smallestMultiple and tests for problem 2
    Also adds command line parsing, including help text.

--- problem2/bin/main.ml
+++ problem2/bin/main.ml
@@ -1,22 +1,82 @@
-open Base
+open Core
 open Lib
 
-let () =
+let test0 () =
   let transitions =
     [|
       [| 2; 0; |];
       [| 1; 1; |];
       [| 2; 1; |];
     |]
-  and int_of_symbol symbol = Dfa.Symbol.(to_int symbol - to_int '0')
   in
   let dfa =
     { Dfa.n = 3;
       symbols = Set.of_sorted_array_unchecked (module Dfa.Symbol) [| '0'; '1'; |];
-      transition = (fun state symbol -> transitions.(state).(int_of_symbol symbol));
+      transition = (fun state symbol -> transitions.(state).(Dfa.Symbol.to_int symbol));
       final = Set.singleton (module Dfa.State) 1;
     }
   in
   match Dfa.shortest_accepted_string dfa with
-  | Some path -> Stdio.print_endline path
+  | Some path -> Stdio.print_endline path (* expect 01 *)
   | None -> Stdio.print_endline "no path"
+
+let test1 () =
+  let dfa = Dfa.accept_decimal_multiples_of 10 in
+  match Dfa.shortest_accepted_string dfa with
+  | Some w -> Stdio.print_endline w (* expect 0 *)
+  | None -> Stdio.print_endline "unreachable"
+
+let test2 () =
+  let k = 26147
+  and digits = Set.of_sorted_array_unchecked (module Dfa.Symbol) [| '1'; '3'; |]
+  in
+  match Dfa.smallest_multiple k digits with
+  | Some w -> Stdio.print_endline w (* expect 1113313113 *)
+  | None -> Stdio.print_endline "unreachable"
+
+let test3 () =
+  let k = 198217
+  and digits = Set.singleton (module Dfa.Symbol) '1'
+  in
+  match Dfa.smallest_multiple k digits with
+  | Some w -> Stdio.print_endline w (* expect a string of 10962 1's *)
+  | None -> Stdio.print_endline "unreachable"
+
+let tests () =
+  test0 ();
+  test1 ();
+  test2 ();
+  test3 ()
+
+let test =
+  Command.basic
+    ~summary:"Run the built-in test functions"
+    (Command.Param.return tests)
+
+let parse_digits digits =
+  digits
+  |> List.filter ~f:Char.is_digit
+  |> Set.of_list (module Dfa.Symbol)
+
+let smallest_multiple =
+  let open Command.Let_syntax in
+  Command.basic
+    ~summary:"Find the smallest multiple of a given integer [k] using only the given digits [digit ...]"
+    [%map_open
+      let k = anon ("k" %: int)
+      and digits = anon (sequence ("digit" %: char))
+      in
+      fun () ->
+        (match Dfa.smallest_multiple k (parse_digits digits) with
+         | Some w -> w
+         | None -> "impossible")
+        |> Stdio.print_endline
+    ]
+
+(* entry point *)
+let () =
+  Command.group
+    ~summary:"CS454 Fall 2018 Project 1 Problem 2"
+    [ "test", test;
+      "smallest-multiple", smallest_multiple ]
+  |> Command.run
--- problem2/lib/dfa.ml
+++ problem2/lib/dfa.ml
@@ -3,7 +3,11 @@ open Base
 module State = Int
 module State_set = Set.M(State) [@@deriving compare]
 
-module Symbol = Char
+module Symbol = struct
+  include Char
+  let to_int c = Char.(to_int c - to_int '0')
+  let of_int x = Char.(of_int (x + to_int '0'))
+end
 module Symbol_set = Set.M(Symbol) [@@deriving compare]
 
 (** The representation of a deterministic finite automaton (DFA).
@@ -19,6 +23,10 @@ type t =
   }
 
 (** Find the shortest input string accepted by a given DFA.
+ *
+ * Implements the "MinString" function.
+ *
+ * Returns an option of the lexicographically-first accepted string.
  *)
 let shortest_accepted_string { n; symbols; transition; final } =
 
@@ -31,7 +39,7 @@ let shortest_accepted_string { n; symbols; transition; final } =
 
   (* Breadth-first search of the state/transition graph.
    *
-   * Returns the shortest path to an accepting state.
+   * Returns an option of the shortest path to an accepting state.
    *)
   let rec bfs current =
     if Set.mem final current then
@@ -57,3 +65,31 @@ let shortest_accepted_string { n; symbols; transition; final } =
   in
 
   Option.(bfs 0 >>| String.of_char_list)
+
+(** Create a DFA for decimal strings that are a multiple of a given integer x.
+ *
+ * The starting state (0) rejects the empty string.
+ * The remaining x states represent the running remainder + 1.
+ * That means 1 is the accepting state, representing a remainder of 0.
+ *)
+let accept_decimal_multiples_of x =
+  let symbols = Set.of_sorted_array_unchecked (module Symbol) (String.to_array "0123456789")
+  in
+  let transition state symbol =
+    let previous_remainder = max 0 (state - 1)
+    and current_digit = Symbol.to_int symbol
+    in
+    let remainder = (previous_remainder * 10 + current_digit) % x
+    in
+    remainder + 1
+  in
+  { n = x + 1; symbols; transition; final = Set.singleton (module State) 1; }
+
+(** Determine the smallest multiple of a given k using only the given digits.
+ *
+ * Implements the "smallestMultiple" function.
+ *
+ * Returns an option of the number as a string of decimal digits.
+ *)
+let smallest_multiple k digits =
+  shortest_accepted_string { (accept_decimal_multiples_of k) with symbols = digits }
